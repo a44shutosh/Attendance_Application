d4c321d2d347199a85cf27853a1c0d1fb547124f
Yash Pahalajani <ypahalajani@gmail.com>
2016-11-12 22:53:43 +0530

    Initial commit

---
+++ Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/Status.java
@@ -0,0 +1,13 @@
+package ashutosh.foodie_guide.attendance_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
