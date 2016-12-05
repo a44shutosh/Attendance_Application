ee6a44647dbf82f378ecd531cd6dca7dde024d13
Yash Pahalajani <ypahalajani@gmail.com>
2016-12-05 13:11:18 +0530

    NFC now registes the roll no. and stores in the MYSQL db

--- Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/activities/Register.java
+++ Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/activities/Register.java
@@ -1,30 +1,36 @@
 package ashutosh.foodie_guide.attendance_application.activities;
 
 import android.content.Intent;
-import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.os.Parcelable;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import java.util.HashMap;
+import java.util.Map;
 
 import ashutosh.foodie_guide.attendance_application.R;
 import ashutosh.foodie_guide.attendance_application.helpers.AESHelper;
+import ashutosh.foodie_guide.attendance_application.utils.Utils;
 
 public class Register extends AppCompatActivity {
 
@@ -61,11 +67,11 @@ public class Register extends AppCompatActivity {
     public void onRegister(View view) {
         String seedValue = "UNBREAK";
         EditText et = (EditText) findViewById(R.id.editTextRegister);
-        String MESSAGE = et.getText().toString();
+        final String MESSAGE = et.getText().toString();
         Log.e("message", "This is the MESSAGE: " + MESSAGE);
 
         String normalText = MESSAGE;
-        String normalTextEnc;
+        String normalTextEnc = null;
         try {
             normalTextEnc = AESHelper.encrypt(seedValue, normalText);
             Log.e("AES", "this is the encrpted text: "+normalTextEnc);
@@ -76,5 +82,52 @@ public class Register extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        final String finalNormalTextEnc = normalTextEnc;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Utils.REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(Register.this,response,Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Register.this,error.toString(),Toast.LENGTH_LONG).show();
+                        NetworkResponse networkResponse = error.networkResponse;
+                        if (networkResponse != null) {
+                            Log.e("Volley", "Error. HTTP Status Code:"+networkResponse.statusCode);
+                        }
+
+                        if (error instanceof TimeoutError) {
+                            Log.e("Volley", "TimeoutError");
+                        }else if(error instanceof NoConnectionError){
+                            Log.e("Volley", "NoConnectionError");
+                        } else if (error instanceof AuthFailureError) {
+                            Log.e("Volley", "AuthFailureError");
+                        } else if (error instanceof ServerError) {
+                            Log.e("Volley", "ServerError");
+                        } else if (error instanceof NetworkError) {
+                            Log.e("Volley", "NetworkError");
+                        } else if (error instanceof ParseError) {
+                            Log.e("Volley", "ParseError");
+                        }
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put(Utils.KEY_ROLL_NO, MESSAGE);
+                params.put(Utils.KEY_TAG_KEY, finalNormalTextEnc);
+                Log.e("Encrypted Value", finalNormalTextEnc);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
--- Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/utils/Utils.java
+++ Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/utils/Utils.java
@@ -1,5 +1,13 @@
 package ashutosh.foodie_guide.attendance_application.utils;
 
 public class Utils{
-    public static final String COURSE_LIST_URL="http://172.22.44.136/nfc/getCourseList.php";
+    public static final String ip="http://172.22.47.160/";
+    public static final String location="nfc/";
+    public static final String COURSE_LIST_URL="http://172.22.47.160/nfc/getCourseList.php";
+
+    public static final String REGISTER_URL = Utils.ip+Utils.location+"register.php";
+
+    public static final String KEY_ROLL_NO = "rollNo";
+    public static final String KEY_TAG_KEY = "tagKey";
+
 }
