b073606a28b10bc1b5ac77e3bdca9adece46a31c
Yash Pahalajani <ypahalajani@gmail.com>
2016-12-06 16:59:41 +0530

    Code added to verify whether the app supports NFC support or not

--- Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/activities/NavDrawer.java
+++ Attendance_Application/app/src/main/java/ashutosh/foodie_guide/attendance_application/activities/NavDrawer.java
@@ -1,9 +1,14 @@
 package ashutosh.foodie_guide.attendance_application.activities;
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +18,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import ashutosh.foodie_guide.attendance_application.R;
 
@@ -45,6 +51,15 @@ public class NavDrawer extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        Context context=getApplicationContext();
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(context);
+        if(nfcAdapter==null){
+            Log.e("NFC", "No support for NFC Hardware");
+        }else{
+            Toast.makeText(context , "NFC Hardware is supported.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
